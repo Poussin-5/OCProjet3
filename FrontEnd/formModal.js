af259244a2e6dd8cee1f6c20201e24760a0d1a45
Poussin-5 <gpautet@hotmail.fr>
2023-08-17 18:36:22 +0200

    oublie du commit ajout de fonctionnalité ajout de projet

--- FrontEnd/formModal.js
+++ FrontEnd/formModal.js
@@ -1,17 +1,71 @@
+import { createElement, createImage } from "./element.js";
+
+const reponse = await fetch("http://localhost:5678/api/works");
+let works = await reponse.json();
+
+let workForm = document.querySelector(".workForm");
+let input = document.querySelector("#input-photo");
+let source = input.files[0];
+
 function submitForm() {
-  let workForm = document.querySelector(".workForm");
   workForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-    let newWork = {
-      title: document.querySelector("[name=title]").value,
-      image: document.querySelector("[name=photo]").value,
-      category: document.querySelector(["name=category"]).value,
-    };
 
-    const reponse = await fetch("http://localhost:5678/api/work", {
+    let formData = new FormData(workForm);
+    console.log(formData);
+
+    const reponse = await fetch("http://localhost:5678/api/works", {
       method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(newWork),
-    });
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+      },
+      body: formData,
+    }).catch((error) => console.log(error));
+
+    console.log(reponse);
+    return reponse;
   });
 }
+
+function importFile() {
+  input.click();
+}
+
+function onClick() {
+  let button = document.querySelector(".input-photo");
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    importFile();
+  });
+}
+
+function loadImage() {
+  input.addEventListener("change", (e) => {
+    source = input.files[0];
+    console.log(source);
+
+    const formPhoto = document.querySelector(".form-photo");
+    formPhoto.style.display = "none";
+
+    const addPhoto = document.querySelector(".add-photo");
+
+    const divLoadedImg = createElement({
+      balise: `div`,
+      classes: "form-photo",
+      id: "loaded-img",
+    });
+    addPhoto.appendChild(divLoadedImg);
+
+    const loadedImg = createImage({
+      src: `../FrontEnd/assets/images/${source.name}`,
+      alt: "photo du projet",
+      classes: "loaded-photo",
+    });
+    divLoadedImg.appendChild(loadedImg);
+  });
+}
+
+submitForm();
+onClick();
+loadImage();
